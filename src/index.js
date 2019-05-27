aa337f29a45e17f9b9270a52999dfbd23f6b500c
Kirankumar Ahir <kirankatariya8778@gmail.com>
2019-05-27 16:33:52 +0530

    Sending location to other clietns

--- src/index.js
+++ src/index.js
@@ -14,9 +14,18 @@ app.use(express.static(publicDirectoryPath));
 
 io.on('connection', socket => {
 	socket.emit('message', 'Welcome!');
+	socket.broadcast.emit('message', 'A new user has joined');
 
 	socket.on('sendMessage', message => {
 		io.emit('message', message);
+	});
+
+	socket.on('disconnect', () => {
+		io.emit('message', 'A user has left');
+	});
+
+	socket.on('sendLocation', ({ latitude, longitude }) => {
+		io.emit('message', `https://google.com/maps?q=${latitude},${longitude}`);
 	});
 });
 
